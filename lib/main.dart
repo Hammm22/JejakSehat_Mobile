ce1c43e2e0137b6f786d9de31c7dd74ca201dade
madebyvin11 <dondilauvin@gmail.com>
2026-06-05 22:22:23 +0700

    yesss

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:jejaksehat_mobile/pages/splash.dart';
+import 'pages/notes_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: SplashScreen(),
+      home: NotesScreen(),
     );
   }
 }
